5296280cf7a0fc348a4f5d5b9711bd609760e40c
Benedykt Powała <benedyktpowala@gmail.com>
2024-11-30 13:00:11 +0100

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
-import { useEffect } from 'react';
 
 const Modal = ({ closeKey, close, renderImage }) => {
-  useEffect(() => {
-    console.log(closeKey);
-    window.document.addEventListener('keydown', closeKey);
-    return window.document.removeEventListener('keydown', closeKey);
-  }, []);
-
   return (
     <div className="overlay" onClick={close}>
       <div className="modal">
